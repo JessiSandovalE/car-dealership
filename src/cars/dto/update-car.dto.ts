d6f7ab7456002a91c82ba9914521f70bc37c7db4
JessicaSandoval <jessica.sandoval@fsfb.org.co>
2023-03-28 21:40:01 -0500

    cars crud

---
+++ src/cars/dto/update-car.dto.ts
@@ -0,0 +1,18 @@
+import { IsString, IsUUID, MinLength , IsOptional} from "class-validator";
+
+export class UpdateCarDto {
+
+  @IsString()
+  @IsUUID()
+  @IsOptional()
+  readonly id?: string;
+
+  @IsString()
+  @IsOptional()
+  readonly brand?: string;
+
+  @IsString()
+  @MinLength(3)
+  @IsOptional()
+  readonly model?: string;
+}
